bdaccf771688f251edd397b0ae6adb14a516504b
Sal Espinosa <sespinos@gmail.com>
2016-06-01 16:14:58 -0600

    Update factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,6 +5,26 @@ FactoryGirl.define do
 
   factory :customer do
     first_name { "Customer" }
-    sequence(:last_name) { |n| "#{n}" } 
+    sequence(:last_name) { |n| "#{n}" }
+  end
+
+  factory :item do
+    merchant
+    sequence(:description) { |n| "Item#{n} description" }
+    unit_price { 450 }
+    name { "Item" }
+  end
+
+  factory :invoice do
+    customer
+    merchant
+    status { "shipped" }
+  end
+
+  factory :invoice_item do
+    item
+    invoice
+    quantity { 1 }
+    unit_price { item.unit_price }
   end
 end
